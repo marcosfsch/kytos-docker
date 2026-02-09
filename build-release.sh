f549c085ea350e6a09a54cc11856c2752bbab157
Marcos Schwarz <marcos.f.sch@gmail.com>
2026-02-09 15:13:29 +0000

    Add build-release.sh script

---
+++ build-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+RELEASE=$1
+
+DOCKERFILE="releases/$RELEASE/Dockerfile"
+test -f $DOCKERFILE || DOCKERFILE=Dockerfile
+
+for repo in $(grep ^ARG $DOCKERFILE |  awk -F "[ =]" '{print $2}' | cut -d_ -f2- | sed 's/python_openflow/python-openflow/; s/kytos_utils/kytos-utils/g'); do curl -s -L   -H "Accept: application/vnd.github+json"   https://api.github.com/repos/kytos-ng/$repo/releases > /tmp/repo-json-$repo; done
+
+ARGS=""
+for repo in $(grep ^ARG $DOCKERFILE |  awk -F "[ =]" '{print $2}' | cut -d_ -f2- | sed 's/python_openflow/python-openflow/; s/kytos_utils/kytos-utils/g'); do ARGS="$ARGS --build-arg branch_"$(echo $repo | tr '-' '_')=$(cat /tmp/repo-json-$repo | jq -r '.[].name' | grep $RELEASE | egrep -v -- '-b[0-9]*$' | sort | tail -n1); done
+ARGS=$(echo $ARGS | sed "s#branch_ui=#release_ui=download/#g")
+
+rm -f /tmp/repo-json-*
+
+
+
+CMD="docker build -f $DOCKERFILE --no-cache -t amlight/kytos:$RELEASE $ARGS ."
+
+bash -x -c "$CMD"
